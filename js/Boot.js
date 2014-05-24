7d8aaae25d40e15e2757dd33702e762bab37a9c6
Sam Russell <samrussell63@gmail.com>
2014-05-23 23:38:44 -0700

    Add mobile viewport enhancements

--- js/Boot.js
+++ js/Boot.js
@@ -1,6 +1,10 @@
-var BasicGame = {};
+var BasicGame = {
 
- 
+    // Add some global vars
+
+};
+
+
 BasicGame.Boot = function (game) {
 
 };
@@ -16,35 +20,58 @@ BasicGame.Boot.prototype = {
     },
 
     create: function () {
-this.game.stage.backgroundColor = '#84CBEC';
-        //  Unless you specifically know your game needs to support multi-touch I would recommend setting this to 1
-        this.input.maxPointers = 1;
 
-        //  Phaser will automatically pause if the browser tab the game is in loses focus. You can disable that here:
+        this.input.maxPointers = 1;
         this.stage.disableVisibilityChange = true;
 
-        if (this.game.device.desktop)
-        {
-            //  If you have any desktop specific settings, they can go in here
+        if (this.game.device.desktop) {
+            this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
+            this.scale.minWidth = 480;
+            this.scale.minHeight = 260;
+            this.scale.maxWidth = 960;
+            this.scale.maxHeight = 320;
             this.scale.pageAlignHorizontally = true;
-        }
-        else
-        {
-            //  Same goes for mobile settings.
-            //  In this case we're saying "scale the game, no lower than 480x260 and no higher than 1024x768"
+            this.scale.pageAlignVertically = true;
+            this.scale.setScreenSize(true);
+        } else {
             this.scale.scaleMode = Phaser.ScaleManager.SHOW_ALL;
             this.scale.minWidth = 480;
             this.scale.minHeight = 260;
             this.scale.maxWidth = 1024;
             this.scale.maxHeight = 768;
-            this.scale.forceLandscape = true;
             this.scale.pageAlignHorizontally = true;
+            this.scale.pageAlignVertically = true;
+            this.scale.forceOrientation(true, false);
+            this.scale.hasResized.add(this.gameResized, this);
+            this.scale.enterIncorrectOrientation.add(this.enterIncorrectOrientation, this);
+            this.scale.leaveIncorrectOrientation.add(this.leaveIncorrectOrientation, this);
             this.scale.setScreenSize(true);
         }
 
-        //  By this point the preloader assets have loaded to the cache, we've set the game settings
-        //  So now let's start the real preloader going
         this.state.start('Preloader');
+
+    },
+
+    gameResized: function (width, height) {
+
+        //  This could be handy if you need to do any extra processing if the game resizes.
+        //  A resize could happen if for example swapping orientation on a device.
+
+    },
+
+    enterIncorrectOrientation: function () {
+
+        BasicGame.orientated = false;
+
+        document.getElementById('orientation').style.display = 'block';
+
+    },
+
+    leaveIncorrectOrientation: function () {
+
+        BasicGame.orientated = true;
+
+        document.getElementById('orientation').style.display = 'none';
 
     }
 
